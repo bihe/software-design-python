26a2e3d59ab11392cf9313f04275308e22974f2a
Henrik Binggl <henrik.binggl@gmail.com>
2024-09-18 21:27:34 +0200

    feat(reservation): fun with the reservation-service and heavy-duty mocking 😅

--- src/restaurant_app/reservation/service.py
+++ src/restaurant_app/reservation/service.py
@@ -1,5 +1,10 @@
+import threading
+from typing import List
+
+from ..restaurant.models import TableModel
 from ..restaurant.service import RestaurantService
-from ..store.entities import TableEntity
+from ..shared.random import random_with_N_digits
+from ..store.entities import ReservationEntity, TableEntity
 from ..store.reservation_repo import ReservationRepository
 from ..store.table_repository import TableRepository
 from .models import ReservationModel, ReservationRequestModel
@@ -11,6 +16,10 @@ class ReservationError(Exception):
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
+
+
+# we need to ensure that we do not have race-conditions during reseration
+mutex = threading.Lock()
 
 
 class ReservationService:
@@ -31,95 +40,138 @@ class ReservationService:
         """Provide a request for a reservation date/time/number-of-people/name.
         The logic needes to find if a table is available for the given date/time-frame
         """
+        reservation: ReservationModel = None
 
-        # the main task is to find a slot for the requested date/time with the given capacity
-        # we will check if the restaurant is open and if there are tables available wilth the given capacity.
-        # for each table which has the capacity, we determine if the table has a reservation slot available.
-        # we do this by checking if the desired from-until time is not reserved, is available.
+        # a parallel reservation process should not interfere
+        with mutex:
 
-        # 1) is the reservation date/time possible with the desired restaurant
-        if not self._restaurant_svc.is_restaurant_open(
-            request.restaurant_id, request.reservation_date, request.time_from, request.time_until
-        ):
-            raise ReservationError("could not place reservation for given date/time; restaurant not open")
+            # the main task is to find a slot for the requested date/time with the given capacity
+            # we will check if the restaurant is open and if there are tables available wilth the given capacity.
+            # for each table which has the capacity, we determine if the table has a reservation slot available.
+            # we do this by checking if the desired from-until time is not reserved, is available.
 
-        # 2) are tables available with the given capacity
-        avail_tables = self._table_repo.get_tables_with_capacity(request.num_people, request.restaurant_id)
-        if avail_tables is None or len(avail_tables) == 0:
-            raise ReservationError("could not place reservation, no tables for the given capactiy are available")
+            # 1) is the reservation date/time possible with the desired restaurant
+            if not self._restaurant_svc.is_restaurant_open(
+                request.restaurant_id, request.reservation_date, request.time_from, request.time_until
+            ):
+                raise ReservationError("could not place reservation for given date/time; restaurant not open")
 
-        table_to_reserve: TableEntity = None
+            # 2) are tables available with the given capacity
+            avail_tables = self._table_repo.get_tables_with_capacity(request.num_people, request.restaurant_id)
+            if avail_tables is None or len(avail_tables) == 0:
+                raise ReservationError("could not place reservation, no tables for the given capactiy are available")
 
-        # 3) is there one table available for reservation (not an overlapping reservation)
-        # iterate over the given tables and find out if the given slot, defined by data/time is available
-        for table in avail_tables:
-            # get the reservations for the given table and date
-            reservations = self._reservation_repo.get_table_reservations_for_date(request.reservation_date, table.id)
-            if len(reservations) == 0:
-                # there are no reservations for the given table -> use the table for reservations
-                table_to_reserve = table
-                break
+            table_to_reserve: TableEntity = None
 
-            # process the saved reservations to find a possible slot
+            # 3) is there one table available for reservation (not an overlapping reservation)
+            # iterate over the given tables and find out if the given slot, defined by data/time is available
+            for table in avail_tables:
+                # get the reservations for the given table and date
+                reservations = self._reservation_repo.get_table_reservations_for_date(
+                    request.reservation_date, table.id
+                )
+                if len(reservations) == 0:
+                    # there are no reservations for the given table -> use the table for reservations
+                    table_to_reserve = table
+                    break
 
-            # +-----------+
-            # |   18:00   |
-            # |<---------------------------------------+-------+------------->
-            # |   19:00   |                            |       |
-            # |<-------------+-------+--+---+----------|-------|------------->
-            # |   20:00   |  |       |  |   |          |       |
-            # |<-------------|-------|--+---+--+---+---+-------+--+-------+-->
-            # |   21:00   |  |       |         |   |              |       |
-            # |<-------------+-------+---------+---+--------------|-------|-->
-            # |   22:00   |                                       |       |
-            # |<--------------------------------------------------+-------+-->
-            # |   23:00   |
-            # +-----------+
+                # process the saved reservations to find a possible slot
 
-            for reservation in reservations:
-                # 1) check if there is a reservation for the exact from-until
-                # above: if the request is from 20:00 to 22:00
-                #           or from 20:00 to 21:00 (within from-until)
-                #           or from 21:00 to 22:00 (within from-until)
-                if request.time_from >= reservation.time_from and request.time_until <= reservation.time_until:
-                    # set any found table to None, because we need to process all reservations
-                    table_to_reserve = None
-                    continue
+                # +-----------+
+                # |   18:00   |
+                # |<---------------------------------------+-------+------------->
+                # |   19:00   |                            |       |
+                # |<-------------+-------+--+---+----------|-------|------------->
+                # |   20:00   |  |       |  |   |          |       |
+                # |<-------------|-------|--+---+--+---+---+-------+--+-------+-->
+                # |   21:00   |  |       |         |   |              |       |
+                # |<-------------+-------+---------+---+--------------|-------|-->
+                # |   22:00   |                                       |       |
+                # |<--------------------------------------------------+-------+-->
+                # |   23:00   |
+                # +-----------+
 
-                # 2) check if we have an overlap
-                # either the until_date is within an existing reservation
-                # above: if the request is from 19:00 to 21:00
-                #           and overlaps with 20:00-22:00
-                if (
-                    request.time_from < reservation.time_from
-                    and request.time_until > reservation.time_from
-                    and request.time_until < reservation.time_until
-                ):
-                    table_to_reserve = None
-                    continue
+                for reservation in reservations:
+                    # 1) check if there is a reservation for the exact from-until
+                    # above: if the request is from 20:00 to 22:00
+                    #           or from 20:00 to 21:00 (within from-until)
+                    #           or from 21:00 to 22:00 (within from-until)
+                    if request.time_from >= reservation.time_from and request.time_until <= reservation.time_until:
+                        # set any found table to None, because we need to process all reservations
+                        table_to_reserve = None
+                        continue
 
-                # or the from_date is within one
-                # above: if the request is from 21:00 to 23:00
-                #           and overlaps with 20:00-22:00
-                if (
-                    request.time_from > reservation.time_from
-                    and request.time_from < reservation.time_until
-                    and request.time_until > reservation.time_until
-                ):
-                    table_to_reserve = None
-                    continue
+                    # 2) check if we have an overlap
+                    # either the until_date is within an existing reservation
+                    # above: if the request is from 19:00 to 21:00
+                    #           and overlaps with 20:00-22:00
+                    if (
+                        request.time_from < reservation.time_from
+                        and request.time_until > reservation.time_from
+                        and request.time_until < reservation.time_until
+                    ):
+                        table_to_reserve = None
+                        continue
 
-                # if we came this far - we got us a table
-                # but it might be, that the table is set to None for the next loop iteration
-                # we need to finish the iteration, because we only look at one reseration after the other
-                # so if we have found a slot in the first iteration, it can be invalidated by the next iteration.
-                table_to_reserve = table
+                    # or the from_date is within one
+                    # above: if the request is from 21:00 to 23:00
+                    #           and overlaps with 20:00-22:00
+                    if (
+                        request.time_from > reservation.time_from
+                        and request.time_from < reservation.time_until
+                        and request.time_until > reservation.time_until
+                    ):
+                        table_to_reserve = None
+                        continue
 
-            # exit the outer loop of tables if we have found a table
-            if table_to_reserve is not None:
-                break
+                    # if we came this far - we got us a table
+                    # but it might be, that the table is set to None for the next loop iteration
+                    # we need to finish the iteration, because we only look at one reseration after the other
+                    # so if we have found a slot in the first iteration, it can be invalidated by the next iteration.
+                    table_to_reserve = table
 
-        if table_to_reserve is None:
-            raise ReservationError("could not place reservation, no tables available for the given date/time")
+                # exit the outer loop of tables if we have found a table
+                if table_to_reserve is not None:
+                    break
 
-        return None
+            if table_to_reserve is None:
+                raise ReservationError("could not place reservation, no tables available for the given date/time")
+
+            # a table is available, store the reservation
+            def work_in_transaction(session) -> List[ReservationModel]:
+                reservation_repo = self._reservation_repo.new_session(session)
+
+                reservation_number = ""
+                while True:
+                    reservation_number = str(random_with_N_digits(7))
+                    if not reservation_repo.is_reservation_number_in_use(reservation_number):
+                        break
+
+                reservation = ReservationEntity(
+                    reservation_date=request.reservation_date,
+                    time_from=request.time_from,
+                    time_until=request.time_until,
+                    people=request.num_people,
+                    reservation_name=request.name,
+                    reservation_number=reservation_number,
+                )
+                reservation.tables.append(table_to_reserve)
+                saved = reservation_repo.save(reservation)
+                return [
+                    ReservationModel(
+                        id=saved.id,
+                        number=saved.reservation_number,
+                        name=saved.reservation_name,
+                        num_people=saved.people,
+                        time_from=saved.time_from,
+                        time_until=saved.time_until,
+                        reservation_date=saved.reservation_date,
+                        reserved_table=TableModel(
+                            id=saved.tables[0].id, number=saved.tables[0].table_number, places=saved.tables[0].seats
+                        ),
+                    )
+                ]
+
+            result = self._reservation_repo.unit_of_work(work_in_transaction)
+            reservation = result[0]
+        return reservation
--- src/restaurant_app/reservation/service_test.py
+++ src/restaurant_app/reservation/service_test.py
@@ -1,4 +1,5 @@
 import datetime
+from typing import List
 from unittest import mock
 
 from ..restaurant.service import RestaurantService
@@ -10,45 +11,75 @@ from .service import ReservationService
 
 
 def test_reservation_table():
-    restaurant_svc_mock = mock.Mock(spec=RestaurantService)
-    restaurant_svc_mock.is_restaurant_open.return_value = True
 
-    reservation_repo_mock = mock.Mock(spec=ReservationRepository)
-    reservation_repo_mock.get_table_reservations_for_date.return_value = [
-        ReservationEntity(
-            reservation_date=datetime.date(2024, 9, 16),
-            time_from=datetime.time(18, 0, 0),
-            time_until=datetime.time(19, 0, 0),
-            people=3,
-            reservation_name="Test1",
-            reservation_number="1",
-        ),
-        ReservationEntity(
-            reservation_date=datetime.date(2024, 9, 16),
-            time_from=datetime.time(20, 30, 0),
-            time_until=datetime.time(22, 0, 0),
-            people=3,
-            reservation_name="Test2",
-            reservation_number="2",
-        ),
-    ]
+    reservation_repo = ReservationRepository(None, None)
 
-    table_repo_mock = mock.Mock(spec=TableRepository)
-    table_repo_mock.get_tables_with_capacity.return_value = [
-        TableEntity(id=1, table_number="1", seats=4, restaurant_id=1)
-    ]
+    def mocked_reservations() -> List[ReservationEntity]:
+        return [
+            ReservationEntity(
+                reservation_date=datetime.date(2024, 9, 16),
+                time_from=datetime.time(18, 0, 0),
+                time_until=datetime.time(19, 0, 0),
+                people=3,
+                reservation_name="Test1",
+                reservation_number="1",
+            ),
+            ReservationEntity(
+                reservation_date=datetime.date(2024, 9, 16),
+                time_from=datetime.time(20, 30, 0),
+                time_until=datetime.time(22, 0, 0),
+                people=3,
+                reservation_name="Test2",
+                reservation_number="2",
+            ),
+        ]
 
-    reservation_svc = ReservationService(
-        restaurant_svc=restaurant_svc_mock, reservation_repo=reservation_repo_mock, table_repo=table_repo_mock
-    )
+    def mocked_save(entity: ReservationEntity) -> ReservationEntity:
+        entity.id = 1
+        return entity
 
-    reservation_svc.reserve(
-        ReservationRequestModel(
-            restaurant_id=1,
-            name="Test",
-            num_people=4,
-            time_from=datetime.time(19, 0, 0),
-            time_until=datetime.time(20, 30, 0),
-            reservation_date=datetime.date(2024, 9, 16),
+    # this is heavy-duty patching of the reservation_repo
+    # we mangle the methods used in the service to behave in a way as we want it
+    # either simply by defining the return_value (= easy)
+    # or by utilizing the option of side_effect, where we can even access the supplied arguments
+    with (
+        mock.patch.object(reservation_repo, "get_table_reservations_for_date", return_value=mocked_reservations()),
+        # side-effect: the suplied argument is a Callable -> call it!
+        mock.patch.object(reservation_repo, "unit_of_work", side_effect=lambda func: func(None)),
+        mock.patch.object(reservation_repo, "new_session", return_value=reservation_repo),
+        mock.patch.object(reservation_repo, "is_reservation_number_in_use", return_value=False),
+        # use the method above to change the supplied value of the save-method
+        mock.patch.object(reservation_repo, "save", side_effect=mocked_save),
+    ):
+        # in this case we ask to crate a MagicMock based on the "template" of the supplied class (=spec)
+        restaurant_svc_mock = mock.Mock(spec=RestaurantService)
+        restaurant_svc_mock.is_restaurant_open.return_value = True
+        # again: mock based on spec
+        table_repo_mock = mock.Mock(spec=TableRepository)
+        table_repo_mock.get_tables_with_capacity.return_value = [
+            TableEntity(id=1, table_number="1", seats=4, restaurant_id=1)
+        ]
+
+        # provide the patched object and two created mocks
+        reservation_svc = ReservationService(
+            restaurant_svc=restaurant_svc_mock, reservation_repo=reservation_repo, table_repo=table_repo_mock
         )
-    )
+
+        reservation = reservation_svc.reserve(
+            ReservationRequestModel(
+                restaurant_id=1,
+                name="Test",
+                num_people=4,
+                time_from=datetime.time(19, 0, 0),
+                time_until=datetime.time(20, 30, 0),
+                reservation_date=datetime.date(2024, 9, 16),
+            )
+        )
+
+        assert reservation.id > 0
+        assert reservation.number != ""  # as this number is random, we cannot compare it to a real value
+        assert len(reservation.number) == 7  # but we know the lenght
+        assert reservation.reserved_table is not None  # we got a table
+        assert reservation.reserved_table.id == 1
+        assert reservation.reserved_table.number == "1"
+        assert reservation.reserved_table.places >= 4
